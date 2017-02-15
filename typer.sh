3f70271c15d580cf2c616753d6c4f7bcc0d93f25
Sr. das Estrelas <lukaswendesson@gmail.com>
2017-02-15 00:17:35 -0400

    Update typer.sh

--- typer.sh
+++ typer.sh
@@ -31,47 +31,7 @@ then
  apt-get upgrade -y
  apt-get install squid3 -y
  apt-get install curl -y
- #Adicionar a porta 443 no ssh e ativar a compressão
- echo "Port 443
- Compression yes" >> /etc/ssh/sshd_config
  
- #Apagar e criar um novo squid.conf
- IP=$(curl https://api.ipify.org/)
- echo $IP
- cd /etc/squid*
-#!/bin/bash
-#Cores
-vermelho="\033[1;31m"
-verde="\033[1;32m"
-amarelo="\033[1;33m"
-azul="\033[1;34m"
-cyan="\033[1;36m"
-b="\033[1;37m"
-cinza="\033[1;30m"
-
-clear
-main()
-#Menu
-echo " "
-echo "$cyan ----------------------------------"
-echo " |  $cyan Script by: Sr. das Estrelas  |"
-echo "$cyan ----------------------------------"
-
-echo "$b
-"[1] Configurar Vps"
-"[2] Atualizar Pacotes"
-"
-echo "$amarelo Escolha uma opcao"
-read resposta
-
-if [ "$resposta" = "1" ]
-then
- echo "$cyan Configurando VPS, aguarde..."
- echo "$verde"
- apt-get update -y
- apt-get upgrade -y
- apt-get install squid3 -y
- apt-get install curl -y
  #Adicionar a porta 443 no ssh e ativar a compressão
  echo "Port 443
  Compression yes" >> /etc/ssh/sshd_config
@@ -109,22 +69,6 @@ via off" > /etc/squid3/squid.conf
 clear
 echo "$cyan IP da VPS: $vermelho $IP"
 echo "$cyan Squid configurado nas portas: $vermelho 80/8080/3128"
- echo ""
- echo ""
- echo "$vermelho Tudo terminado crie um usuario para testar :)"
-fi
-
-if [ "$resposta" = "2" ]
-then
- echo "$cyan Atualizando pacotes"
- echo "$b"
- apt-get update
- echo "$vermelho Tudo terminado :)"
-fi
-
-echo "$cyan IP da VPS: $vermelho $IP"
-echo "$cyan Squid configurado nas portas: $vermelho 80/8080/3128"
-
  echo ""
  echo ""
  echo "$vermelho Tudo terminado crie um usuario para testar :)"
