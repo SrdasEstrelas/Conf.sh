87348874eab5cc1d7bd029a6ccd783ba2a8a26f8
Sr. das Estrelas <lukaswendesson@gmail.com>
2017-03-23 08:30:56 -0300

    Create PackDDoS.sh

---
+++ PackDDoS.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /home
+
+#Socks
+wget https://raw.githubusercontent.com/SrdasEstrelas/Conf.sh/master/socks.py
+
+#Socks 2
+wget https://github.com/SrdasEstrelas/Conf.sh/blob/master/socks.pyc?raw=true
+
+#Terminal
+wget https://raw.githubusercontent.com/SrdasEstrelas/Conf.sh/master/terminal.py
+
+#Terminal 2
+wget https://github.com/SrdasEstrelas/Conf.sh/blob/master/terminal.pyc?raw=true
+
+#ThorShammer
+https://raw.githubusercontent.com/SrdasEstrelas/Conf.sh/master/torshammer.py
+
+chmod a+x /home/torshammer.py
+chmod a+x /home/terminal.py
+chmod a+x /home/socks.py
+chmod a+x /home/terminal.pyc
+chmod a+x /home/socks.pyc
